ee1740a652321b68778bff4ead73f6a16c7d1700
Gie05 <70033068+Gie05@users.noreply.github.com>
2020-08-26 20:45:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://workable-first-armadillo.glitch.me"]
+const urls = ["https://cheezy-music.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
